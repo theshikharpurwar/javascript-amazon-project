8434562d9bacee583bd448e911e51848d0bfb983
theshikharpurwar <spurwar2002@gmail.com>
2024-02-28 22:22:50 +0530

    made different tests

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -25,7 +25,7 @@ export function renderOrderSummary() {
     );
 
     cartSummaryHTML += `
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: ${dateString}
             </div>
@@ -40,7 +40,7 @@ export function renderOrderSummary() {
                 <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                     Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>

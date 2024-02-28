b43151499f63643eeab583916bf1efe6824209f9
theshikharpurwar <spurwar2002@gmail.com>
2024-02-28 17:23:45 +0530

    create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-    cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-        deliveryOptionId: '1'
-    }, {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId: '2'
-    }];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if(!cart) {
+        cart = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+        }, {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+            deliveryOptionId: '2'
+        }];
+    }
 }
 
 export function calculateCartQuantity(){
@@ -39,7 +45,7 @@ export function addToCart(productId){
             productId: productId,
             quantity: Number(document.querySelector(`.js-quantity-selector-${productId}`).value),
             deliveryOptionId: '1'
-        })
+        });
         document.querySelector('.added-to-cart').classList.add('js-added-to-cart');
     }
     saveToStorage();
